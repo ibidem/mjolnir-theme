b05a45f03ab6c96478f2719651eeb365285a40c4
srcspider <source.spider@gmail.com>
2012-04-25 11:41:47 +0300

    updating defaults

--- +App/config/ibidem/themes.php
+++ +App/config/ibidem/themes.php
@@ -2,10 +2,10 @@
 	(
 		'theme.default' => 'classic',
 		'style.default' => 'default',
-		'themes.dir'    => 'themes', # root, relative to App/
-		'themes.config' => '!config',
+		'themes.dir'    => 'themes', # root, relative to +App/
+		'themes.config' => '+config',
 		'style.dir.default' => 'Styles',
-		'style.config'  => '!config',
+		'style.config'  => '+config',
 		'script.dir.default' => 'Scripts',
-		'script.config' => '!config',
+		'script.config' => '+config',
 	);
