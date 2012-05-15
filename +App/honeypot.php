33499b442ec8fad1a1279e6dc480251d4d64846e
srcspider <source.spider@gmail.com>
2012-05-15 13:09:45 +0300

    updated honeypot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -4,5 +4,8 @@
 
 // HowTo: minion honeypot -n "ibidem\\theme"
 
-class Layer_Theme extends \ibidem\theme\Layer_Theme {}
-class ThemeView extends \ibidem\theme\ThemeView {}
+class Controller_Mockup extends \ibidem\theme\Controller_Mockup { /** @return \ibidem\theme\Controller_Mockup */ static function instance() { return parent::instance(); } }
+class Layer_Theme extends \ibidem\theme\Layer_Theme { /** @return \ibidem\theme\Layer_Theme */ static function instance() { return parent::instance(); } }
+class Task_Make_Style extends \ibidem\theme\Task_Make_Style { /** @return \ibidem\theme\Task_Make_Style */ static function instance($encoded_task = null) { return parent::instance($encoded_task); } }
+class Task_Make_Theme extends \ibidem\theme\Task_Make_Theme { /** @return \ibidem\theme\Task_Make_Theme */ static function instance($encoded_task = null) { return parent::instance($encoded_task); } }
+class ThemeView extends \ibidem\theme\ThemeView { /** @return \ibidem\theme\ThemeView */ static function instance() { return parent::instance(); } }
