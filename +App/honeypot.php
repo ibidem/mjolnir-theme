f929d4e1677ce05adc6de1207f3556837fe55bd7
srcspider <source.spider@gmail.com>
2012-08-28 18:02:10 +0300

    updated honepot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -1,8 +1,9 @@
 <?php namespace app;
 
-// This is a IDE honeypot. :)
+// This is an IDE honeypot. It tells IDEs the class hirarchy, but otherwise has
+// no effect on your application. :)
 
-// HowTo: minion honeypot -n "ibidem\\theme"
+// HowTo: order honeypot -n 'ibidem\theme'
 
 class Controller_Mockup extends \ibidem\theme\Controller_Mockup { /** @return \ibidem\theme\Controller_Mockup */ static function instance() { return parent::instance(); } }
 class Layer_Theme extends \ibidem\theme\Layer_Theme { /** @return \ibidem\theme\Layer_Theme */ static function instance() { return parent::instance(); } }
