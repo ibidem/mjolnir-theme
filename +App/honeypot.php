011c6654a9fa416af6532e464e013476e5fcdd8f
srcspider <source.spider@gmail.com>
2012-09-10 02:12:30 +0300

    updated honeypot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -3,13 +3,13 @@
 // This is an IDE honeypot. It tells IDEs the class hirarchy, but otherwise has
 // no effect on your application. :)
 
-// HowTo: order honeypot -n 'ibidem\theme'
+// HowTo: order honeypot -n 'mjolnir\theme'
 
 class Context_Exception_NotAllowed extends \mjolnir\theme\Context_Exception_NotAllowed { /** @return \mjolnir\theme\Context_Exception_NotAllowed */ static function instance() { return parent::instance(); } }
 class Context_Exception_NotApplicable extends \mjolnir\theme\Context_Exception_NotApplicable { /** @return \mjolnir\theme\Context_Exception_NotApplicable */ static function instance() { return parent::instance(); } }
 class Context_Exception_NotFound extends \mjolnir\theme\Context_Exception_NotFound { /** @return \mjolnir\theme\Context_Exception_NotFound */ static function instance() { return parent::instance(); } }
 class Context_Exception_Unknown extends \mjolnir\theme\Context_Exception_Unknown { /** @return \mjolnir\theme\Context_Exception_Unknown */ static function instance() { return parent::instance(); } }
-class Context_Style extends \mjolnir\theme\Context_Style { /** @return \mjolnir\theme\Context_Style */ static function instance() { return parent::instance(); } }
+class Context_Ref extends \mjolnir\theme\Context_Ref { /** @return \mjolnir\theme\Context_Ref */ static function instance() { return parent::instance(); } }
 class Controller_Mockup extends \mjolnir\theme\Controller_Mockup { /** @return \mjolnir\theme\Controller_Mockup */ static function instance() { return parent::instance(); } }
 class Layer_Theme extends \mjolnir\theme\Layer_Theme { /** @return \mjolnir\theme\Layer_Theme */ static function instance() { return parent::instance(); } }
 class Task_Make_Style extends \mjolnir\theme\Task_Make_Style { /** @return \mjolnir\theme\Task_Make_Style */ static function instance($encoded_task = null) { return parent::instance($encoded_task); } }
