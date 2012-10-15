bed593879feac2ec4374cedcf98cf5ebaa4caa42
srcspider <source.spider@gmail.com>
2012-10-15 14:47:30 +0300

    mockup config

---
+++ +App/config/mjolnir/mockup.php
@@ -0,0 +1,82 @@
+<?php return array
+	(
+		'lorempixel:defaults' => array
+			(
+				'category' => 'technics',
+				'grayscale' => true,
+			),
+		'given_names' => array
+			(
+				//// male
+				
+				// UK
+				'Oliver', 'Jack', 'Harry', 'Alfie', 'Charlie', 'Thomas', 'William', 'Joshua', 'George', 'James',
+				// France
+				'Nathan', 'Lucas', 'Jules', 'Enzo', 'Gabriel', 'Louis', 'Arthur', 'Raphaël', 'Mathis', 'Ethan',
+				// Germany
+				'Ben', 'Leon', 'Lukas', 'Fynn', 'Jonas', 'Maximilian', 'Paul', 'Felix', 'Luka',
+				// Italy
+				'Francesco', 'Alessandro', 'Andrea', 'Lorenzo', 'Matteo', 'Mattia', 'Gabriele', 'Riccardo', 'Davide', 'Leonardo',
+				
+				//// female
+			
+				// UK
+				'Olivia', 'Sophie', 'Emily', 'Lily', 'Amelia', 'Jessica', 'Ruby', 'Chloe', 'Grace', 'Evie',
+				// France
+				'Olivia', 'Sophie', 'Emily', 'Amelia', 'Jessica', 'Ruby', 'Chloe',
+				// Germany
+				'Mia', 'Emma', 'Hannah', 'Anna', 'Leah', 'Lina', 'Marie', 'Sophia', 'Lena',
+				// Italy
+				'Giulia', 'Sara', 'Martina', 'Giorgia', 'Chiara', 'Aurora', 'Alice', 'Emma', 'Alessia',
+			),
+	
+		'family_names' => array
+			(
+				// Irish
+				'Murphy', 'Kelly', 'O\'Sullivan', 'Breathnach', 'Mac Gabhann', 'Ó Briain', 'Ó Néill', 'Mac Cárthaigh', 'Lynch',
+				// UK
+				'Brown', 'Smith', 'Patel', 'Roberts', 'Khan', 'Cox', 'Davis', 'Clarke', 'Hall', 'Thompson', 'Jenkins', 'Griffiths', 'Morgan', 'Evans', 'Hughes',
+				// France
+				'Dubois', 'Durand', 'Bernard', 'Martin', 'Lefebvre', 'Leroy', 'Laurent', 'Lefèvre', 'André', 'Roux', 'Vincent', 'Fournier', 'Moreau',
+				// Germany
+				'Müller', 'Schmidt', 'Schneider', 'Fischer', 'Weber', 'Meyer', 'Schulz', 'Wagner', 'Becker', 'Hoffmann',
+				// Italy
+				'Rossi', 'Russo', 'Ferrari', 'Bianchi', 'Ricci', 'Bruno', 'Bruno', 'Rizzo', 'Moretti', 'Santoro',
+			),
+	
+		'punctuation' => array
+			(
+				'.', '?', '.', '!', '.', ';'
+			),
+	
+		'words' => array
+			(
+				'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'quisque', 'vitae', 'metus', 
+				'non', 'leo', 'rutrum', 'interdum', 'eget', 'tincidunt', 'erat', 'morbi', 'id', 'lorem', 'id', 'turpis', 
+				'blandit', 'gravida', 'et', 'vitae', 'neque', 'nam', 'pretium', 'tempus', 'mauris', 'in', 'porta', 'orci', 
+				'dictum', 'vitae', 'mauris', 'orci', 'tortor', 'aliquam', 'sit', 'amet', 'iaculis', 'ut', 'mattis', 'in', 
+				'augue', 'quisque', 'purus', 'nunc', 'egestas', 'quis', 'fringilla', 'ac', 'ornare', 'eget', 'est', 'vestibulum', 
+				'ante', 'ipsum', 'primis', 'in', 'faucibus', 'orci', 'luctus', 'et', 'ultrices', 'posuere', 'cubilia', 'curae', 
+				'mauris', 'leo', 'orci', 'pulvinar', 'sit', 'amet', 'imperdiet', 'vitae', 'laoreet', 'vel', 'augue', 'phasellus', 
+				'id', 'varius', 'nulla', 'nulla', 'vitae', 'magna', 'elit', 'vestibulum', 'vitae', 'ullamcorper', 'elit', 'sed', 
+				'eu', 'enim', 'sem', 'at', 'mattis'
+			),
+	
+		'cities' => array
+			(
+				'New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia',
+				'Phoenix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose',
+				'Jacksoville', 'Indianapolis', 'Austin', 'San Francisco',
+				'Columbus', 'Fort Worth', 'Charlotte', 'Detroit', 'El Paso',
+				'Memphis', 'Boston', 'Seattle', 'Denver', 'Baltimore', 
+				'Washington', 'Nashville', 'Louisville', 'Milwaukee',
+				'Portland', 'Oklahoma City', 'Las Vegas', 'Albuquerque',
+				'Tucson', 'Fresno', 'Sacramento', 'Long Beach', 'Kansas City',
+				'Mesa', 'Virginia Beach', 'Atlanta', 'Colorado Springs',
+				'Raleigh', 'Omaha', 'Miami', 'Tulsa', 'Oakland', 'Cleveland',
+				'Minneapolis', 'Wichita', 'Arlington', 'New Orleans', 
+				'Bakersfield', 'Tampa', 'Anaheim', 'Honolulu', 'Aurora', 
+				'Santa Ana', 'St. Louis', 'Riverside',
+			),
+	
+	);
