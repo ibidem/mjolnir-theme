8b6bf6f245575f89c38cffe5495ae7622d211caf
srcspider <source.spider@gmail.com>
2013-05-10 15:30:46 +0300

    formatting++

--- ThemeDriver/StyleResource.php
+++ ThemeDriver/StyleResource.php
@@ -29,6 +29,8 @@ class ThemeDriver_StyleResource extends \app\Instantiatable implements \mjolnir\
 		$mimetype = \app\Filesystem::mimetype($resourcepath);
 
 		$this->channel()->add('http:header', ['Content-Type', $mimetype]);
+		
+		// cache headers
 		$this->channel()->add('http:header', ['Cache-Control', 'private']);
 		$this->channel()->add('http:header', ['Expires', \date(DATE_RFC822, \strtotime("7 days"))]);
 
